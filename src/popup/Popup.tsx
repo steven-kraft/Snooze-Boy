8c166d6cb561a159d6e112ef7ada0a1b02dfb8cc
Steven-Kraft <steven.kraftjp@gmail.com>
2019-08-25 21:43:16 -0400

    Added Basic Buttons

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -5,6 +5,14 @@ interface AppProps {}
 
 interface AppState {}
 
+function Button(props) {
+  let img = "img/icon-" + props.index + ".svg"
+  return <button className="snooze-button">
+    <div className="icon"><img src={img} /></div>
+    <div className="label">1 Hour</div>
+    </button>;
+}
+
 export default class Popup extends React.Component<AppProps, AppState> {
     constructor(props: AppProps, state: AppState) {
         super(props, state);
@@ -18,7 +26,15 @@ export default class Popup extends React.Component<AppProps, AppState> {
     render() {
         return (
             <div className="popupContainer">
-                Hello, world!
+                <Button index="1" />
+                <Button index="2" />
+                <Button index="3" />
+                <Button index="4" />
+                <Button index="5" />
+                <Button index="6" />
+                <Button index="7" />
+                <Button index="8" />
+                <Button index="9" />
             </div>
         )
     }
