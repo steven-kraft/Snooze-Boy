6480749f22da82ca6368c8c664157dd3b0c13f3c
Steven Kraft <steven.kraftjp@gmail.com>
2020-02-08 15:34:53 -0500

    Added Time Select field to Button Edit

--- src/popup/components/ButtonEdit.tsx
+++ src/popup/components/ButtonEdit.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
 import IconPreview from './IconPreview'
+import TextField from '@material-ui/core/TextField';
 
 interface ButtonEditProps {
   button: any;
+}
+
+
+function TimePicker(props) {
+  return <TextField
+      id="time"
+      type="time"
+      defaultValue="07:30"
+      className="time-picker"
+      InputLabelProps={{
+        shrink: true,
+      }}
+      inputProps={{
+        step: 300, // 5 min
+      }}
+    />;
 }
 
 export default class ButtonEdit extends React.Component<ButtonEditProps> {
@@ -15,6 +32,7 @@ export default class ButtonEdit extends React.Component<ButtonEditProps> {
       <div className={"button-editor"}>
         <IconPreview icon={`icon-${this.props.button.index}.svg`} />
         <input className="snooze-label-edit" type="text" placeholder={this.props.button.label} name="label"/>
+        <TimePicker />
       </div>
     );
   }
